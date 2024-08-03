f7d7a998f8388a5506ff1a7da9fae16c13be455f
Vasyl Holubovskyi <holvascompany@gmail.com>
2024-08-03 16:48:20 +0200

    add styled components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React gh-pages template
-    </div>
-  );
+import React from 'react';
+import { Container, FirstTitle, SecondTitle, Button } from './App.styled.js';
+
+function App() {
+const [count, setCount] = React.useState(0); // React Hook useState, в який передаємо змінну count та функцію оновлення значення змінної setCount
+
+const onClickPlus = () => {
+  setCount(count + 1);
 };
+
+const onClickMinus = () => {
+  setCount(count - 1);
+};
+
+  return (
+    <Container>
+      <div>
+        <FirstTitle>Лічильник:</FirstTitle>
+        <SecondTitle>{count}</SecondTitle>
+        <Button onClick={onClickMinus} className="minus">- Мінус</Button>
+        <Button onClick={onClickPlus} className="plus">Плюс +</Button>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
